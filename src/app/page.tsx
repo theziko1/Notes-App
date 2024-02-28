e3b9dbe3eca4c2a8cf718153aad9df39414254cc
Anas <adelanas2000@gmail.com>
2024-02-28 12:50:39 +0100

    Created Note Page And Update Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,108 +1,94 @@
-import Image from "next/image";
+import Link from "next/link";
+import { FaPen } from "react-icons/fa";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { MdDeleteForever } from "react-icons/md";
+
+interface NotesInterface {
+  name: string;
+  position: string;
+  createdAt: string;
+  color: string;
+}
 
 export default function Home() {
+  const Notes: NotesInterface[] = [
+    {
+      name: "Jhon Doe",
+      position: "Jr. Web Developer",
+      createdAt: "May 22, 2025",
+      color: "#FFA500",
+    },
+    {
+      name: "Jane Smith",
+      position: "Sr. Web Developer",
+      createdAt: "May 22, 2025",
+      color: "#89CFF0",
+    },
+    {
+      name: "Alex Johnson",
+      position: "Frontend Developer",
+      createdAt: "May 22, 2025",
+      color: "#BF40BF",
+    },
+    {
+      name: "Emily Brown",
+      position: "UI/UX Designer",
+      createdAt: "May 22, 2025",
+      color: "#FFC300",
+    },
+    {
+      name: "Sarah Wilson",
+      position: "Backend Developer",
+      createdAt: "May 22, 2025",
+      color: "#FFA500",
+    },
+    {
+      name: "Micheal Davis",
+      position: "Fullstack Developer",
+      createdAt: "May 22, 2025",
+      color: "#BF40BF",
+    },
+  ];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="min-h-screen px-24 py-10 font-[Poppins]">
+      <header className="w-full flex items-center gap-10 mb-12">
+        <h1 className="font-semibold">Notes App</h1>
+        <input
+          type="text"
+          placeholder="Search ..."
+          className="w-96 h-12 rounded-full border border-[#777777] pl-5 font-medium outline-none"
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </header>
+      <section className="flex items-center gap-10 mb-12">
+        <Link href="addnotes">
+          <IoMdAddCircleOutline className="w-14 h-14 cursor-pointer" />
+        </Link>
+        <h1 className="font-bold text-4xl">NOTES</h1>
+      </section>
+      <section className="w-full flex items-center justify-center">
+        <div className=" flex items-center gap-14 flex-wrap">
+          {Notes.map((note, index) => (
+            <div
+              key={index}
+              className="p-5 flex flex-col justify-between w-[21rem] h-[18rem] overflow-y-auto bg-[#FFA500] rounded-md"
+            >
+              <div className="gap-3 flex flex-col">
+                <h1 className="font-bold text-2xl">{note.name}</h1>
+                <p className="font-normal text-lg">{note.position}</p>
+              </div>
+              <div className="flex items-center justify-between">
+                <p className="font-semibold">{note.createdAt}</p>
+                <div className="flex items-center gap-3">
+                  <Link href="updateNotes">
+                    <FaPen className="w-7 h-7 cursor-pointer hover:scale-125 duration-500"/>
+                  </Link>
+                  <MdDeleteForever className="w-8 h-8 cursor-pointer hover:scale-125 duration-500"/>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     </main>
   );
 }
